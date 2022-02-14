9e0bfbc070e57c871b2e1d628bae71d1615c8ce8
Dayu Wang <dwang@stchas.edu>
2022-02-14 16:37:44 -0600

    evening.lecture_1 Added

---
+++ src/evening/lecture_1/CuboidVolume.java
@@ -0,0 +1,27 @@
+// Created by Dayu Wang (dwang@stchas.edu) on 2022-02-14
+
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-02-14
+
+
+package evening.lecture_1;
+
+import java.util.Scanner;
+
+public class CuboidVolume {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Create a scanner.
+        // Let the user input required information.
+        System.out.print("Please enter the length of the cuboid: ");
+        int length = scanner.nextInt();
+        System.out.print("Please enter the width of the cuboid: ");
+        int width = scanner.nextInt();
+        System.out.print("Please enter the height of the cuboid: ");
+        int height = scanner.nextInt();
+        // Calculate the volume of the cuboid based on the user input data.
+        int volume = length * width * height;
+        // Write the result to the standard console.
+        System.out.println("Volume: " + volume);
+        // Close the scanner.
+        scanner.close();
+    }
+}
---
+++ src/evening/lecture_1/HelloWorld.java
@@ -0,0 +1,12 @@
+// Created by Dayu Wang (dwang@stchas.edu) on 2022-02-14
+
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-02-14
+
+
+package evening.lecture_1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
