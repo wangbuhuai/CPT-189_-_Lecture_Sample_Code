4fe4805bf8bb274e1a3fe6bd047326bb77b2f1b4
Dayu Wang <dwang@stchas.edu>
2022-08-21 17:07:44 -0500

    online.lecture_1 added

---
+++ src/online/lecture_1/HelloWorld.java
@@ -0,0 +1,14 @@
+// Created by Dayu Wang (dwang@stchas.edu) on 2022-08-21
+
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-08-21
+
+
+package online.lecture_1;
+
+public class HelloWorld {
+    // The "main()" method marks the beginning of the program.
+    public static void main(String[] args) {
+        // Use "System.out.println" or "System.out.print" to write something to the console.
+        System.out.println("Hello World");
+    }
+}
