2044cd4510b9b6f99fae968189973b1fc3f20bef
Dayu Wang <dwang@stchas.edu>
2022-10-03 16:47:09 -0500

    online.lecture_6 added

---
+++ src/online/lecture_6/Pair.java
@@ -0,0 +1,61 @@
+// Created by Dayu Wang (dwang@stchas.edu) on 2021-04-17
+
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-10-03
+
+
+package online.lecture_6;
+
+/** A pair of an integer and a string */
+public class Pair {
+    // Data fields
+    private int first;  // Stores the integer (first value) in the pair.
+    private String second;  // Stores the string (second value) in the pair.
+
+    // Constructors
+
+    public Pair() { second = ""; }  // Default constructor
+
+    public Pair(int first, String second) {  // Constructor with initial values in the pair
+        this.first = first;
+        this.second = second;
+    }
+
+    // Getters
+
+    /** Returns the first value (integer) in the pair.
+        @return: first value (integer) in the pair
+    */
+    public int getFirst() { return first; }
+
+    /** Returns the second value (string) in the pair.
+        @return: second value (string) in the pair
+    */
+    public String getSecond() { return second; }
+
+    // Setters
+
+    /** Updates the first value (integer) in the pair.
+        @param first: updated first value (integer) in the pair
+    */
+    public void setFirst(int first) { this.first = first; }
+
+    /** Updates the second value (string) in the pair.
+        @param second: updated second value (string) in the pair
+    */
+    public void setSecond(String second) { this.second = second; }
+
+    // Methods
+
+    /** Customizes the output format of the pair.
+        @return: a string representing the output format of the pair
+    */
+    @Override
+    public String toString() { return String.format("(%d, %s)", first, second); }
+
+    /** Creates a Pair object from an integer and a string.
+        @param first: first value (integer) in the pair
+        @param second: second value (string) in the pair
+        @return: a Pair object formed by the first and second values
+    */
+    public static Pair makePair(int first, String second) { return new Pair(first, second); }
+}
---
+++ src/online/lecture_6/Rectangle.java
@@ -0,0 +1,70 @@
+// Created by Dayu Wang (dwang@stchas.edu) on 2021-04-17
+
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-10-03
+
+
+package online.lecture_6;
+
+/** A geometric rectangle */
+public class Rectangle {
+    // Data fields
+    private int width;  // Stores the width of the rectangle.
+    private int height;  // Stores the height of the rectangle.
+
+    // Constructors
+
+    public Rectangle() {}  // Default constructor
+
+    public Rectangle(int width, int height) {  // Constructor with initial width and height
+        this.width = width;
+        this.height = height;
+    }
+
+    // Getters
+
+    /** Returns the width of the rectangle.
+        @return: width of the rectangle
+    */
+    public int getWidth() { return width; }
+
+    /** Returns the height of the rectangle.
+        @return: height of the rectangle
+    */
+    public int getHeight() { return height; }
+
+    // Setters
+
+    /** Updates the width of the rectangle.
+        @param width: updated width of the rectangle
+    */
+    public void setWidth(int width) { this.width = width; }
+
+    /** Updates the height of the rectangle.
+        @param height: updated height of the rectangle
+    */
+    public void setHeight(int height) { this.height = height; }
+
+    // Methods
+
+    /** Calculates the area of the rectangle.
+        @return: calculated area of the rectangle
+    */
+    public int area() { return width * height; }
+
+    /** Calculates the perimeter of the rectangle.
+        @return: calculated perimeter of the rectangle
+    */
+    public int perimeter() { return 2 * (width + height); }
+
+    /** Customizes the output format of the rectangle.
+        @return: a string representing the output format of the rectangle
+    */
+    @Override
+    public String toString() { return String.format("{width: %d, height: %d}", width, height); }
+
+    /** Tests whether this rectangle is smaller than another rectangle.
+        @param other: another rectangle to compare with this rectangle
+        @return: {true} if this rectangle has smaller area than the other rectangle; {false} otherwise
+    */
+    public boolean isSmallerThan(Rectangle other) { return this.area() < other.area(); }
+}
